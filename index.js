08eab58c41603979a33038db4a12c49bca03bab7
haroog96 <harshitg996@gmail.com>
2023-02-01 21:52:05 -0500

    Skeleton functionality added

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mysql = require('mysql');
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Packersvsvikings1!",
+  database: "response"
+});
+
+con.connect((err)=>{
+  if(err){
+    throw err;
+  }
+  console.log("Connected...."); 
+});
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/search", (req, res) => {
+  const searchValue= req.query.search;
+  // Query the database
+  con.query(
+    "SELECT message FROM response.gptresponse WHERE path = 1 AND path_num = ?",
+    [searchValue],    
+    (error, results, fields) => {
+      if (error) {
+        console.error("Error: " + error);
+        res.status(500).send("Error: " + error);
+        return;
+      }
+      if (results.length > 0) {
+        res.send(JSON.parse(JSON.stringify(results))[0].message);
+      } else {
+        res.send("No results found");
+      }
+    }
+  );
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
